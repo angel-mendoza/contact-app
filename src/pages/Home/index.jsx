547e005a33a9ae640e367b977cbeb5426c8aa872
Angel Mendoza <mendozaangel003@gmail.com>
2022-04-30 18:23:40 -0500

    basic structure of a react project

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+const HomePage = () => {
+  return (
+    <h1>Homa Page</h1>
+  )
+}
+
+export default HomePage
---
+++ src/routes/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from '../pages/Home/index.jsx'
+import NotFound from '../pages/NotFound/index.jsx'
+
+const App = () => {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
