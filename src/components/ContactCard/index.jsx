76b6a962e6f66dd314073bf01961b637c60a7fd3
Angel Mendoza <mendozaangel003@gmail.com>
2022-05-01 17:01:02 -0500

    ContactCard ready

---
+++ src/components/ContactCard/index.jsx
@@ -0,0 +1,69 @@
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import { red } from '@mui/material/colors'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import MailIcon from '@mui/icons-material/Mail'
+import PhoneIcon from '@mui/icons-material/Phone'
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props
+  return <IconButton {...other} />
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest
+  })
+}))
+
+const ContactCard = ({ contact }) => {
+  const [expanded, setExpanded] = React.useState(false)
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded)
+  }
+
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            { contact.firstName.substr(0, 1) }
+          </Avatar>
+        }
+        title={`${contact.firstName} ${contact.lastName}`}
+        subheader="September 14, 2016"
+      />
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+            <MailIcon /> {contact.email}
+          </Typography>
+          <Typography paragraph>
+            <PhoneIcon /> {contact.phone}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  )
+}
+
+export default ContactCard
--- src/components/ContactItem/index.jsx
+++ src/components/ContactItem/index.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import { styled } from '@mui/material/styles'
-import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary
-}))
+import ContactCard from '@/components/ContactCard'
 
 const ContactItem = ({ contact }) => {
   return (
     <Grid item xs={12} md={4}>
-      <Item>{contact.firstName}</Item>
+      <ContactCard contact={contact} />
     </Grid>
   )
 }
