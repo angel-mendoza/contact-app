c25588f8679ff3a0f560c42892ebe54f659d8b78
Angel Mendoza <mendozaangel003@gmail.com>
2022-05-01 22:29:04 -0500

    add pagination

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 
+import Pagination from '@mui/material/Pagination'
 import Fab from '@mui/material/Fab'
 import AddIcon from '@mui/icons-material/Add'
 
 import ContactList from '@/components/ContactList'
 import ContactItem from '@/components/ContactItem'
 import PageComponent from '@/components/PageComponent'
-import '@/styles/PageComponent.scss'
 
 import { useDispatch, useSelector } from '@/store'
 import { getContact } from '@/slices/contact'
@@ -16,12 +16,20 @@ const HomePage = () => {
   const dispatch = useDispatch()
   const stateContact = useSelector((state) => state.contacts)
 
-  const getData = async () => {
+  const initialiceData = () => {
     dispatch(getContact(stateContact.currentPage))
   }
 
+  const getData = (value) => {
+    dispatch(getContact(value))
+  }
+
+  const handleChangePagination = (event, newPage) => {
+    getData(newPage)
+  }
+
   useEffect(() => {
-    getData()
+    initialiceData()
   }, [])
 
   return (
@@ -42,6 +50,7 @@ const HomePage = () => {
       >
         <AddIcon />
       </Fab>
+      <Pagination className='pagination' xs={12} count={stateContact.totalPages} page={stateContact.currentPage} onChange={handleChangePagination} />
     </PageComponent>
   )
 }
