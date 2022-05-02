2ebdf295b229d6c16d3215f983f02534d51a41b3
Angel Mendoza <mendozaangel003@gmail.com>
2022-05-01 22:01:05 -0500

    refactor home and  add route to fab

--- src/components/PageComponent/index.jsx
+++ src/components/PageComponent/index.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
 
-import {
-  useTheme,
-  Typography,
-  useMediaQuery
-} from '@mui/material'
-
 import '@/styles/PageComponent.scss'
 
 const PageComponent = (props) => {
-  const theme = useTheme()
-  const mobileDevice = useMediaQuery(theme.breakpoints.down('sm'))
   return (
     <div className='page-component'>
-      <Typography
-        align="left"
-        variant={mobileDevice ? 'h5' : 'h4'}
-      >
-        {props.title}
-      </Typography>
       {props.children}
     </div>
   )
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+
 import Fab from '@mui/material/Fab'
 import AddIcon from '@mui/icons-material/Add'
 
 import ContactList from '@/components/ContactList'
 import ContactItem from '@/components/ContactItem'
 import PageComponent from '@/components/PageComponent'
+import '@/styles/PageComponent.scss'
 
 import { useDispatch, useSelector } from '@/store'
 import { getContact } from '@/slices/contact'
@@ -22,7 +25,7 @@ const HomePage = () => {
   }, [])
 
   return (
-    <PageComponent title="My contact list">
+    <PageComponent>
       <ContactList contacts={stateContact}>
         {
           stateContact.results.map(contact => (
@@ -30,7 +33,13 @@ const HomePage = () => {
           ))
         }
       </ContactList>
-      <Fab className='fab' color="primary" aria-label="add">
+      <Fab
+        className='fab'
+        color="primary"
+        aria-label="add"
+        component={RouterLink}
+        to="/create"
+      >
         <AddIcon />
       </Fab>
     </PageComponent>
